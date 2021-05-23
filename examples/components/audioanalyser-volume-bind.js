4315d0374ec44fdc15f0118aba01d1dbda6d2057
ryota-mitarai <ryotamitarai0@gmail.com>
2021-05-23 14:57:09 -0400

    chore: update waveform example

--- examples/components/audioanalyser-volume-bind.js
+++ examples/components/audioanalyser-volume-bind.js
@@ -3,11 +3,11 @@
  */
 AFRAME.registerComponent('audioanalyser-volume-bind', {
   schema: {
-    analyserEl: {type: 'selector'},
-    component: {type: 'string'},
-    property: {type: 'string'},
-    max: {type: 'number'},
-    multiplier: {type: 'number'},
+    analyserEl: { type: 'selector' },
+    component: { type: 'string' },
+    property: { type: 'string' },
+    max: { type: 'number' },
+    multiplier: { type: 'number' },
   },
 
   tick: function () {
@@ -17,9 +17,11 @@ AFRAME.registerComponent('audioanalyser-volume-bind', {
     var value;
 
     analyserComponent = data.analyserEl.components.audioanalyser;
-    if (!analyserComponent.analyser) { return; }
+    if (!analyserComponent.analyser) {
+      return;
+    }
 
     value = Math.min(data.max, analyserComponent.volume * data.multiplier);
     el.setAttribute(data.component, data.property, value);
-  }
+  },
 });
